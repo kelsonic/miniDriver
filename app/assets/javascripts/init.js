b900aa3b5de5f7b031711bd337927e40d73e54e9
kelsonic <kgadams93@gmail.com>
2016-06-06 11:05:07 -0700

    Add ajax requests

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
   $(".button-collapse").sideNav();
   $('.materialboxed').materialbox();
+
+  $("#on").on("click", function(e) {
+    e.preventDefault()
+
+    $.ajax({
+      url: "192.16.51.218",
+      method: "POST",
+      data: "ON"
+    }).done(function(msg) {
+      console.log(msg);
+    }).fail(function(err) {
+      console.log(err);
+    })
+  })
+
+  $("#off").on("click", function(e) {
+    e.preventDefault()
+    
+    $.ajax({
+      url: "192.16.51.218",
+      method: "POST",
+      data: "OFF"
+    }).done(function(msg) {
+      console.log(msg);
+    }).fail(function(err) {
+      console.log(err);
+    })
+  })
 })
