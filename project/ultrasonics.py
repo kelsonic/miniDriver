22ba0a04deb16d5e5966634b9274a5efb687a125
kelsonic <kgadams93@gmail.com>
2016-06-05 21:49:15 -0700

    Add ultrasonics python script

---
+++ project/ultrasonics.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+import time
+import socket
+
+# create socket and bind host
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('172.16.51.218', 9009))
+
+GPIO.setmode(GPIO.BOARD)
+
+TRIG = 7
+ECHO = 12
+
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.output(TRIG, 0)
+
+GPIO.setup(ECHO, GPIO.IN)
+
+print "Starting measurement..."
+
+try:
+    while 1:
+
+        GPIO.output(TRIG, 1)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, 0)
+        
+        while GPIO.input(ECHO) == 0:
+            pass
+        start = time.time()
+
+        while GPIO.input(ECHO) == 1:
+            pass
+        stop = time.time()
+
+        distance = (stop - start) * 17000
+        time.sleep(0.1)
+        if distance <= 20:
+            print "STOP"
+            client_socket.send("STOP OBSTACLE")
+        else:
+            print "GO"
+
+except KeyboardInterrupt:
+    GPIO.cleanup() 
