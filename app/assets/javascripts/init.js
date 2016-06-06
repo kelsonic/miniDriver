d5fdd5f4aad8f92cbbe30d9da058e95c55fb5a08
kelsonic <kgadams93@gmail.com>
2016-06-06 12:19:58 -0700

    Complete ajax call to initiate the car to go or turn it off

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -2,30 +2,38 @@ $(document).ready(function() {
   $(".button-collapse").sideNav();
   $('.materialboxed').materialbox();
 
-  $("#on").on("click", function(e) {
+  $(".on-off-buttons").on("click", "#on", function(e) {
     e.preventDefault()
 
     $.ajax({
       url: "http://172.16.50.242:9009",
       method: "POST",
-      data: "ON"
-    }).done(function(msg) {
+      data: "ONN"
+    })
+
+    .done(function(msg) {
       console.log(msg);
-    }).fail(function(err) {
+    })
+
+    .fail(function(err) {
       console.log(err);
     })
   })
 
-  $("#off").on("click", function(e) {
+  $(".on-off-buttons").on("click", "#off", function(e) {
     e.preventDefault()
 
     $.ajax({
       url: "http://172.16.50.242:9009",
       method: "POST",
       data: "OFF"
-    }).done(function(msg) {
+    })
+
+    .done(function(msg) {
       console.log(msg);
-    }).fail(function(err) {
+    })
+
+    .fail(function(err) {
       console.log(err);
     })
   })
